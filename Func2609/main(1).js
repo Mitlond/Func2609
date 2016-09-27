6017c845511ae9c425d6b63f3652532b4832f14a
Mitlond <comp122012016@mail.ru>
2016-09-27 10:14:19 +0300

    Function

---
+++ Func2609/main(1).js
@@ -0,0 +1,28 @@
+﻿//1 function
+var sum = function sum(a,b){
+var sum=a+b;
+return sum;
+}
+alert(sum(2,3));
+//2 function
+function sum (a,b){
+alert(a+b);
+return s;
+}
+alert(sum(1,3));
+//3 function 
+var kls=(function (x){return x*x;}(10));
+alert (kls);
+//4 function 
+var crose=(function (a,b){return a+b;}(2,2));
+alert (crose);
+//5 function 
+function gip(ab,bc){
+return math.sqrt((ab+ab)+(bc+bc));
+}
+console.log(gip);
+//6 function
+/*var gip=(function(ab,bc){
+return math.sqrt((ab+ab)+(bc+bc));
+}
+console.log(gip(3,4));*/
---
+++ Func2609/main.js
@@ -0,0 +1,12 @@
+function recrut (a,b,c);{
+if a==0 return a/b;
+var d = x1,x2;
+if (d<0)return a/b;
+if (d=0)return b/c;
+if (d>0)return a/c;
+{
+x1=1,7658;
+x2=3,0543;
+}
+}
+console.log(d);
